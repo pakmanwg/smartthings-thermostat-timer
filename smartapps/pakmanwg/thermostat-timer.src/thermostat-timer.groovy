853e6f8c338baa1c4787852da35941cc170e2f4f
pakmanwg <pakmanw@sbcglobal.net>
2018-02-11 14:01:28 -0800

    Create thermostat-timer.groovy

---
+++ smartapps/pakmanwg/thermostat-timer.src/thermostat-timer.groovy
@@ -0,0 +1,99 @@
+/**
+ *  Copyright 2017 pakmanw@sbcglobal.net
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Thermostat Timer
+ *
+ *  Author: pakmanw@sbcglobal.net
+ *
+ *  Change Log
+ *  2017-9-17  - v01.01 Created
+ *  2017-11-25 - v01.02 Add Switch Off function, single target temperature
+ *
+ */
+
+definition(
+    name: "Thermostat Timer",
+    namespace: "pakmanwg",
+    author: "pakmanw@sbcglobal.net",
+    description: "Turn off Themostat after certain amount of time",
+    category: "Green Living",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/GreenLiving/Cat-GreenLiving.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/GreenLiving/Cat-GreenLiving@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/GreenLiving/Cat-GreenLiving@3x.png"
+)
+
+preferences {
+    section("When switch on..."){
+	    input "switch1", "capability.switch"
+    }
+    section("When button pushed..."){
+	    input "button1", "capability.button"
+    }
+    section("Choose Thermostat(s)"){
+	    input "thermostat1", "capability.thermostat"
+    }
+    section("Minutes..."){
+        input "minutes", "number", range: "1..*", title: "Minutes", required: true
+    }
+    section("Set Target temperature") {
+        input "opSet", "decimal", title: "Temperature", required: true
+    }
+    section("Default temperatures") {
+        input "defHeatSet", "decimal", title: "When Heating", description: "Default Heating temperature"
+        input "defCoolSet", "decimal", title: "When Cooling", description: "Default Cooling temperature"
+    }
+}
+
+def installed() {
+    log.debug "Installed with settings: ${settings}"
+    subscribe(switch1, "switch.on", turnOnThermostat)
+    subscribe(button1, "button.pushed", turnOnThermostatb)
+    subscribe(switch1, "switch.off", turnOffThermostat)
+    state.lastStatus = "off"
+}
+
+def updated(settings) {
+    log.debug "Updated with settings: ${settings}"
+    unsubscribe()
+    subscribe(switch1, "switch.on", turnOnThermostat)
+    subscribe(button1, "button.pushed", turnOnThermostatb)
+    subscribe(switch1, "switch.off", turnOffThermostat)
+}
+
+def turnOnThermostat(evt) {
+    state.lastStatus = "on"
+    if (opSet < thermostat1.currentValue("temperature")) {
+        thermostat1.setCoolingSetpoint(opSet)
+    } else {
+        thermostat1.setHeatingSetpoint(opSet)
+    }
+    def delay = 60 * minutes
+    runIn(delay, switchOff)
+}
+
+def turnOnThermostatb(evt) {
+    if (state.lastStatus == "off") {
+        switch1.On()
+    } else {
+        switch1.Off()
+    }
+}
+
+def turnOffThermostat() {
+    state.lastStatus = "off"
+    thermostat1.setCoolingSetpoint(defCoolSet)
+    thermostat1.setHeatingSetpoint(defHeatSet)
+}
+
+def switchOff() {
+    switch1.Off()
+}
